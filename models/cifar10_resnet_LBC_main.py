2ed2e604c277ffb393a058090725aa5e48679031
jianhao <jianhao2@illinois.edu>
2017-12-02 18:30:00 -0600

    jianhao, LBC resnet completed

--- models/cifar10_resnet_LBC_main.py
+++ models/cifar10_resnet_LBC_main.py
@@ -9,22 +9,283 @@
 """
 This function will run the LBC resnet on cifar10.
 """
-class TEMP_opt:
-    def __init__(self):
-        self.nClass = 10
-        self.stride = 1
-        self.sparsity = 0.9
-        self.nInputPlane = 3
-        self.numChannels = 128 # number of intermediate layers between blocks, i.e. nChIn
-        self.number_of_b = 512 # number of binary filters in LBC, i.e. nChTmp
-        self.full = 512 # number of hidden units in FC
-        self.convSize = 3 # LB convolutional filter size
-        self.depth = 20 # number of blocks
-        self.weightDecay = 1e-4
-        self.LR = 1e-4 #initial learning rate
-        self.nEpochs = 0 # number of total epochs to run
-        self.epochNumber = 1 # manual epoch number
-        self.batchSize = 20
-        self.data_format = None
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from TEMP_opt import TEMP_opt
+
+import argparse
+import os
+import sys
+
+import tensorflow as tf
+
+import resnet_LBC
+
+_HEIGHT = 32
+_WIDTH = 32
+_DEPTH = 3
+_NUM_CLASSES = 10
+_NUM_DATA_FILES = 5
+
+# We use a weight decay of 0.0002, which performs better than the 0.0001 that
+# was originally suggested.
+_WEIGHT_DECAY = 2e-4
+_MOMENTUM = 0.9
+
+_NUM_IMAGES = {
+    'train': 50000,
+    'validation': 10000,
+}
 
 
+def record_dataset(filenames):
+  """Returns an input pipeline Dataset from `filenames`."""
+  record_bytes = _HEIGHT * _WIDTH * _DEPTH + 1
+  return tf.data.FixedLengthRecordDataset(filenames, record_bytes)
+
+
+def get_filenames(is_training, data_dir):
+  """Returns a list of filenames."""
+  data_dir = os.path.join(data_dir, 'cifar-10-batches-bin')
+
+  assert os.path.exists(data_dir), (
+      'Run cifar10_download_and_extract.py first to download and extract the '
+      'CIFAR-10 data.')
+
+  if is_training:
+    return [
+        os.path.join(data_dir, 'data_batch_%d.bin' % i)
+        for i in range(1, _NUM_DATA_FILES + 1)
+    ]
+  else:
+    return [os.path.join(data_dir, 'test_batch.bin')]
+
+
+def parse_record(raw_record):
+  """Parse CIFAR-10 image and label from a raw record."""
+  # Every record consists of a label followed by the image, with a fixed number
+  # of bytes for each.
+  label_bytes = 1
+  image_bytes = _HEIGHT * _WIDTH * _DEPTH
+  record_bytes = label_bytes + image_bytes
+
+  # Convert bytes to a vector of uint8 that is record_bytes long.
+  record_vector = tf.decode_raw(raw_record, tf.uint8)
+
+  # The first byte represents the label, which we convert from uint8 to int32
+  # and then to one-hot.
+  label = tf.cast(record_vector[0], tf.int32)
+  label = tf.one_hot(label, _NUM_CLASSES)
+
+  # The remaining bytes after the label represent the image, which we reshape
+  # from [depth * height * width] to [depth, height, width].
+  depth_major = tf.reshape(
+      record_vector[label_bytes:record_bytes], [_DEPTH, _HEIGHT, _WIDTH])
+
+  # Convert from [depth, height, width] to [height, width, depth], and cast as
+  # float32.
+  image = tf.cast(tf.transpose(depth_major, [1, 2, 0]), tf.float32)
+
+  return image, label
+
+
+def preprocess_image(image, is_training):
+  """Preprocess a single image of layout [height, width, depth]."""
+  if is_training:
+    # Resize the image to add four extra pixels on each side.
+    image = tf.image.resize_image_with_crop_or_pad(
+        image, _HEIGHT + 8, _WIDTH + 8)
+
+    # Randomly crop a [_HEIGHT, _WIDTH] section of the image.
+    image = tf.random_crop(image, [_HEIGHT, _WIDTH, _DEPTH])
+
+    # Randomly flip the image horizontally.
+    image = tf.image.random_flip_left_right(image)
+
+  # Subtract off the mean and divide by the variance of the pixels.
+  image = tf.image.per_image_standardization(image)
+  return image
+
+
+def input_fn(is_training, data_dir, batch_size, num_epochs=1):
+  """Input_fn using the tf.data input pipeline for CIFAR-10 dataset.
+
+  Args:
+    is_training: A boolean denoting whether the input is for training.
+    data_dir: The directory containing the input data.
+    batch_size: The number of samples per batch.
+    num_epochs: The number of epochs to repeat the dataset.
+
+  Returns:
+    A tuple of images and labels.
+  """
+  dataset = record_dataset(get_filenames(is_training, data_dir))
+
+  if is_training:
+    # When choosing shuffle buffer sizes, larger sizes result in better
+    # randomness, while smaller sizes have better performance. Because CIFAR-10
+    # is a relatively small dataset, we choose to shuffle the full epoch.
+    dataset = dataset.shuffle(buffer_size=_NUM_IMAGES['train'])
+
+  dataset = dataset.map(parse_record)
+  dataset = dataset.map(
+      lambda image, label: (preprocess_image(image, is_training), label))
+
+  dataset = dataset.prefetch(2 * batch_size)
+
+  # We call repeat after shuffling, rather than before, to prevent separate
+  # epochs from blending together.
+  dataset = dataset.repeat(num_epochs)
+
+  # Batch results by up to batch_size, and then fetch the tuple from the
+  # iterator.
+  dataset = dataset.batch(batch_size)
+  iterator = dataset.make_one_shot_iterator()
+  images, labels = iterator.get_next()
+
+  return images, labels
+
+def cifar10_model_fn(features, labels, mode, params):
+  """Model function for CIFAR-10."""
+  print('Called cifar10_model_fn!')
+  print('params in cifar10_model_fn = {}'.format(params))
+  tf.summary.image('images', features, max_outputs=6)
+
+  # network = resnet_model.cifar10_resnet_v2_generator(
+  #     params['resnet_size'], _NUM_CLASSES, params['data_format'],
+  #     number_of_b = params['number_of_binary_mask'], sparsity = params['sparsity'],
+  #     shared_weights = params['shared_weights'])
+
+  network = resnet_LBC.cifar10_resnet_LBC_generator(
+          depth = params['depth'], nClass = params['nClass'],
+          kSize = params['kSize'], numChannels = params['numChannels'],
+          units_in_FC = params['units_in_FC'], data_format = params['data_format'],
+          number_of_b = params['number_of_b'], sparsity = params['sparsity'],
+          shared_weights = params['shared_weights'])
+
+  inputs = tf.reshape(features, [-1, _HEIGHT, _WIDTH, _DEPTH])
+  logits = network(inputs, mode == tf.estimator.ModeKeys.TRAIN)
+
+  predictions = {
+      'classes': tf.argmax(logits, axis=1),
+      'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
+  }
+
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+  # Calculate loss, which includes softmax cross entropy and L2 regularization.
+  cross_entropy = tf.losses.softmax_cross_entropy(
+      logits=logits, onehot_labels=labels)
+
+  # Create a tensor named cross_entropy for logging purposes.
+  tf.identity(cross_entropy, name='cross_entropy')
+  tf.summary.scalar('cross_entropy', cross_entropy)
+
+  # Add weight decay to the loss.
+  loss = cross_entropy + _WEIGHT_DECAY * tf.add_n(
+      [tf.nn.l2_loss(v) for v in tf.trainable_variables()])
+
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    # Scale the learning rate linearly with the batch size. When the batch size
+    # is 128, the learning rate should be 0.1.
+    initial_learning_rate = 0.1 * params['batch_size'] / 128
+    batches_per_epoch = _NUM_IMAGES['train'] / params['batch_size']
+    global_step = tf.train.get_or_create_global_step()
+
+    # Multiply the learning rate by 0.1 at 100, 150, and 200 epochs.
+    boundaries = [int(batches_per_epoch * epoch) for epoch in [100, 150, 200]]
+    values = [initial_learning_rate * decay for decay in [1, 0.1, 0.01, 0.001]]
+    learning_rate = tf.train.piecewise_constant(
+        tf.cast(global_step, tf.int32), boundaries, values)
+
+    # Create a tensor named learning_rate for logging purposes
+    tf.identity(learning_rate, name='learning_rate')
+    tf.summary.scalar('learning_rate', learning_rate)
+
+    optimizer = tf.train.MomentumOptimizer(
+        learning_rate=learning_rate,
+        momentum=_MOMENTUM)
+
+    # Batch norm requires update ops to be added as a dependency to the train_op
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+      train_op = optimizer.minimize(loss, global_step)
+  else:
+    train_op = None
+
+  accuracy = tf.metrics.accuracy(
+      tf.argmax(labels, axis=1), predictions['classes'])
+  metrics = {'accuracy': accuracy}
+
+  # Create a tensor named train_accuracy for logging purposes
+  tf.identity(accuracy[1], name='train_accuracy')
+  tf.summary.scalar('train_accuracy', accuracy[1])
+
+  return tf.estimator.EstimatorSpec(
+      mode=mode,
+      predictions=predictions,
+      loss=loss,
+      train_op=train_op,
+      eval_metric_ops=metrics)
+
+
+def main(unused_argv):
+  # Using the Winograd non-fused algorithms provides a small performance boost.
+  os.environ['TF_ENABLE_WINOGRAD_NONFUSED'] = '1'
+
+  # Set up a RunConfig to only save checkpoints once per training cycle.
+  run_config = tf.estimator.RunConfig().replace(save_checkpoints_secs=1e9)
+  params = {
+          'depth': FLAGS.depth,
+          'nClass': FLAGS.nClass,
+          'kSize': FLAGS.convSize,
+          'numChannels': FLAGS.numChannels,
+          'units_in_FC': FLAGS.full,
+          'data_format': FLAGS.data_format,
+          'number_of_b': FLAGS.number_of_b,
+          'sparsity': FLAGS.sparsity,
+          'shared_weights': FLAGS.shared_weights,
+          'batch_size': FLAGS.batch_size
+          }
+  print('params in main: \n {}'.format(params))
+
+  cifar_classifier = tf.estimator.Estimator(
+      model_fn=cifar10_model_fn, model_dir=FLAGS.model_dir, config=run_config,
+      params = params)
+
+  for _ in range(FLAGS.train_epochs // FLAGS.epochs_per_eval):
+    tensors_to_log = {
+        'learning_rate': 'learning_rate',
+        'cross_entropy': 'cross_entropy',
+        'train_accuracy': 'train_accuracy'
+    }
+
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=100)
+
+    cifar_classifier.train(
+        input_fn=lambda: input_fn(
+            True, FLAGS.data_dir, FLAGS.batch_size, FLAGS.epochs_per_eval),
+        hooks=[logging_hook])
+
+    # Evaluate the model and print results
+    eval_results = cifar_classifier.evaluate(
+        input_fn=lambda: input_fn(False, FLAGS.data_dir, FLAGS.batch_size))
+    print("here is eval")
+    print(eval_results)
+
+
+if __name__ == '__main__':
+  tf.logging.set_verbosity(tf.logging.INFO)
+  FLAGS = TEMP_opt()
+  FLAGS.model_dir = '../trained_model/resnet/'
+  FLAGS.data_dir = '../data/cifar10'
+  FLAGS.train_epochs = 200
+  FLAGS.epochs_per_eval = 5
+  print(FLAGS)
+  tf.app.run()
+#  
+# 
--- models/resnet_LBC.py
+++ models/resnet_LBC.py
@@ -19,11 +19,12 @@ import tensorflow as tf
 import numpy as np
 from TEMP_opt import TEMP_opt
 
+# these two params are use in batch norm
 _BATCH_NORM_DECAY = 0.997
 _BATCH_NORM_EPSILON = 1e-5
 
-opt = TEMP_opt()
-print('opt.batchSize = {}'.format(opt.batchSize))
+# opt = TEMP_opt()
+# print('opt.batchSize = {}'.format(opt.batchSize))
 
 def batch_norm_relu(inputs, is_training, data_format):
   """Performs a batch normalization followed by a ReLU."""
@@ -37,18 +38,17 @@ def batch_norm_relu(inputs, is_training, data_format):
   return inputs
 
 def random_binary_convlution(inputs, nChIn, nChOut, kW, kH, dW, dH, 
-        sparsity, padding, data_format):
+        padding, data_format, sparsity, shared_weights):
     """
     inputs: a tensor
     nChIn/nChOut: number of input/output channels
     kW/kH : kernel size in LBC
     dW/dH : stride size in LBC
+    padding : str, padding or not in RBC.
     # padW/padH: padding size
     sparsity: proportion of non zero elements in ancher weights
-    padding : str, padding or not in RBC.
+    shared_weights: boolean, whether uses same binary filter or not.
     """
-    # ancher_shape = np.array([opt.convSize, opt.convSize,
-    #                          opt.numChannels, opt.number_of_b])
     ancher_shape = np.array([kW, kH, nChIn, nChOut])
     num_elements = np.product(ancher_shape, dtype = int)
     num_non_zero = num_elements * sparsity
@@ -57,7 +57,7 @@ def random_binary_convlution(inputs, nChIn, nChOut, kW, kH, dW, dH,
     #initialize ancher weights
     ancher_weights = np.zeros(shape = ancher_shape, dtype = np.float)
     ancher_weights = np.reshape(ancher_weights, newshape = [num_elements])
-    if opt.shared_weights:
+    if shared_weights:
         np.random.seed(42)
     index_non_zero = np.random.choice(num_elements, num_non_zero, replace = False)
     for i in index_non_zero:
@@ -65,7 +65,7 @@ def random_binary_convlution(inputs, nChIn, nChOut, kW, kH, dW, dH,
     ancher_weights = np.reshape(ancher_weights, newshape = ancher_shape)
     ancher_weights_tensor = tf.constant(ancher_weights, dtype = tf.float32)
 
-    if opt.data_format == 'channels_first':
+    if data_format == 'channels_first':
         tf_format = 'NCHW'
         tf_strides = [1, 1, dW, dH]
     else:
@@ -74,53 +74,64 @@ def random_binary_convlution(inputs, nChIn, nChOut, kW, kH, dW, dH,
     
     diff_map = tf.nn.conv2d(inputs, filter = ancher_weights_tensor,
                             strides = tf_strides, padding = padding,
-                            data_format = data_format)
+                            data_format = tf_format)
     return diff_map
     
 
-# def basic_block(inputs, is_training, n_ChIn, n_ChOut, filter_size, strides, 
-#         data_format, number_of_b, sparsity, shared_weights, name):
-def basic_block(inputs, nChIn, nChTmp, kSize, sparsity, is_training, 
-        data_format, block_name):
+def basic_block_LBC(inputs, nChIn, nChTmp, kSize, is_training, data_format, 
+        sparsity, shared_weights, block_name):
     """
     basic resnet block, with LBC module replacement.
-    input channels -> nChIn
-    intermediate channels -> nChTmp, which is just number_of_b
-    output channels -> nChIn
+    nChIn : number of input channels to the block. Notice that the in/out of a
+            block has the same 'depth'/number of channels
+    nChTmp: number of binary filters used in the block. Cancel out by the 
+            second conv in block.
+    kSize:  filter size in RBC.
+    is_training: a boolean, tell if training
+    data_format: 'channels_first' or 'channels_last'
+    sparsity/shared_weights: params used in RBC.
+    block_name: string. name of block.
     """
     with tf.name_scope(block_name):
         shortcut = inputs
         with tf.name_scope('batch_normalization'):
             inputs = batch_norm_relu(inputs, is_training, data_format)
         with tf.name_scope('random_binary_conv'):
-            inputs = random_binary_convlution(inputs, nChIn = nChIn, nChOut = nChTmp, 
-                    kW = kSize, kH = kSize, dW = 1, dH = 1, sparsity = sparsity,
-                    padding = 'SAME', data_format = data_format)
+            inputs = random_binary_convlution(inputs, nChIn = nChIn, nChOut = nChTmp,
+                    kW = kSize, kH = kSize, dW = 1, dH = 1, padding = 'SAME',
+                    data_format = data_format, sparsity = sparsity,
+                    shared_weights = shared_weights)
 
         inputs = tf.nn.relu(inputs)
-        with tf.name_scope('1x1 conv'):
+        with tf.name_scope('1x1_conv'):
             # the second conv doesn't need any padding, since it's 1x1.
             inputs = tf.layers.conv2d(inputs = inputs, filters = nChIn,
                                     kernel_size = [1, 1],
-                                    paddind = 'valid',
+                                    padding = 'valid',
                                     data_format = data_format,
                                     use_bias = False)
         output = shortcut + inputs
     return output
 
-def cifar10_resnet_LBC_generator(opt):
+def cifar10_resnet_LBC_generator(depth, nClass, kSize, numChannels, 
+        units_in_FC, data_format, number_of_b, sparsity, shared_weights):
     """
+    depth: how many blocks to use in resnet.
+    nClass: how many classes in the output layer
+    kSize: convolution size in resnet.
+    numChannels: how many filters to use in the first conv layer.
+                 i.e. number of input channels to the blocks chain.
+    units_in_FC: number of units in the first fully connected layer.
+    data_format: 'channels_first' or 'channels_last'
+    number_of_b: number of binary filters. i.e. the filters used in RBC
+    sparsity/shared_weights: params used in LBC
     returns a model function that takes inputs and is_training and compute the output
     tensor.
     """
-    kSize = opt.convSize
-    nInputPlane = opt.nInputPlane
-    nChIn = opt.numChannels
-    nChTmp = opt.number_of_b
-    sparsity = opt.sparsity
-    data_format = opt.data_format
-    shared_weights = opt.shared_weights
-    depth = opt.depth
+    nChIn = numChannels
+    nChTmp = number_of_b
+    # after a 5x5 non-overlapping average pooling, the cifar10 image origin
+    # size is 32x32, and now is only 6x6 left.
     shape_after_avg = 6 * 6
     
     if data_format is None:
@@ -141,24 +152,27 @@ def cifar10_resnet_LBC_generator(opt):
         # inputs = batch_norm_relu(inputs, is_training, data_format)
         
         for i in range(1, depth):
-            block_name = 'residual block ' + str(i)
-            inputs = basic_block(inputs, nChIn, nChTmp, kSize, sparsity, is_training,
-                    data_format, block_name = block_name)
+            block_name = 'LBC_residual_block_' + str(i)
+            inputs = basic_block_LBC(inputs, nChIn, nChTmp, kSize, is_training,
+                    data_format = data_format, sparsity = sparsity,
+                    shared_weights = shared_weights, block_name = block_name)
 
-        # inputs = batch_norm_relu(inputs, is_training, data_format)
+        inputs = batch_norm_relu(inputs, is_training, data_format)
         inputs = tf.layers.average_pooling2d(inputs, pool_size = 5,
                 strides = 5, padding = 'valid', data_format = data_format)
         inputs = tf.identity(inputs, name = 'final_avg_pool')
         
         # a two layer FC network with dropout while training.
-        inputs = tf.reshape(inputs, [-1, shape_after_avg])
+        inputs = tf.reshape(inputs, [-1, numChannels * shape_after_avg])
         inputs = tf.layers.dropout(inputs, training = is_training)
-        inputs = tf.layers.dense(inputs, units = opt.full, activation = tf.nn.relu)
+        inputs = tf.layers.dense(inputs, units = units_in_FC, activation = tf.nn.relu)
         inputs = tf.layers.dropout(inputs, training = is_training)
-        inputs = tf.layers.dense(inputs, units = opt.nClass)
+        inputs = tf.layers.dense(inputs, units = nClass)
         inputs = tf.identity(inputs, name = 'final_dense_out')
         return inputs
 
     return model
 
-cifar10_resnet_LBC_generator(opt)
+# cifar10_resnet_LBC_generator(depth = 2, nClass = 3, kSize = 3, numChannels = 8,
+#         units_in_FC = 10, data_format = None, number_of_b = 5, sparsity = 0.5,
+#         shared_weights = False)
