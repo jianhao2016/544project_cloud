b4c0932fcf068ae45b6af07757a8e5a8a46df488
jianhao <jianhao2@illinois.edu>
2017-12-02 18:31:17 -0600

    jianhao, add hard-coded opt, should be deleted afterwards

---
+++ models/TEMP_opt.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 qizai <jianhao2@illinois.edu>
+#
+# Distributed under terms of the MIT license.
+
+"""
+This is just a temperary file.
+"""
+class TEMP_opt:
+    def __init__(self):
+        self.nClass = 10
+        self.stride = 1
+        self.sparsity = 0.9
+        self.nInputPlane = 3
+        self.numChannels = 128 # number of intermediate layers between blocks, i.e. nChIn
+        self.number_of_b = 512 # number of binary filters in LBC, i.e. nChTmp
+        self.full = 512 # number of hidden units in FC
+        self.convSize = 3 # LB convolutional filter size
+        self.depth = 20 # number of blocks
+        self.weightDecay = 1e-4
+        self.LR = 1e-4 #initial learning rate
+        self.nEpochs = 0 # number of total epochs to run
+        # self.epochNumber = 1 # manual epoch number
+        self.batch_size = 20
+        self.data_format = None
+        self.shared_weights = False
+
+
